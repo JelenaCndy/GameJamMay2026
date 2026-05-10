91aebfd12acd24f6c9d0c95c56663f754549cbdc
Hertz <johnhenryhertz@gmail.com>
2026-05-10 15:09:50 +0200

    aegjklradfgijoprs

--- Assets/ApplyGameData.cs
+++ Assets/ApplyGameData.cs
@@ -1,20 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ApplyGameData : MonoBehaviour
 {
-
+    public TextMeshProUGUI m_deliveryCount;
     GameManager m_gameManager;
     // Start is called before the first frame update
-    void Start()
+
+
+    private void Awake()
+    {
+        m_gameManager = GameManager.Instance;
+    }
+    void Start()    
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public string FormatString()
     {
-        
+
+        string temp = $"Current Score: {m_gameManager.m_currentDeliveryCount}";
+        return temp;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        m_deliveryCount.text = FormatString();
     }
 }
